63156d1f7e8e6e9577e6ded00a97c870700d7fd0
Joan Manuel Vasquez <yoloprogramo22@gmail.com>
2018-08-04 10:23:01 -0400

    Update server.js

--- src/config/server.js
+++ src/config/server.js
@@ -9,7 +9,7 @@ const app = express();
 app.set('port', process.env.PORT || 8080);
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization");
+	res.header("Access-Control-Allow-Headers", "origin, content-type, x-access-token");
 	res.header("Access-Control-Allow-Credentials", "true");
 	res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
     next();
